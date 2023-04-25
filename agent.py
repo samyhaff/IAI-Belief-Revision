63e1f7e86253dfa7a5a3eda4942bef8708b525eb
andreabosisio <andrea2.bosisio@mail.polimi.it>
2023-04-25 21:28:24 +0200

    Two versions of meet partial contraction. Both behaves correctly on contracting {(A|B), (A&B)} with {B}

--- agent.py
+++ agent.py
@@ -1,5 +1,6 @@
 from sympy import symbols, And, Or, Not
-from sympy.logic.boolalg import to_int_repr, to_cnf, to_dnf
+from sympy.logic.boolalg import to_int_repr, to_cnf
+from itertools import chain, combinations
 
 
 class Agent:
@@ -7,7 +8,11 @@ class Agent:
         self.knowledge_base = set()
 
     def tell(self, sentence):
+        #print('Adding', sentence, 'as', to_cnf(sentence))
+        #self.knowledge_base.add(to_cnf(sentence))
+        print('Adding', sentence)
         self.knowledge_base.add(sentence)
+        print('New knowledge base:', self.knowledge_base)
 
     def ask(self, query):
         return self.resolution(query)
@@ -21,8 +26,11 @@ class Agent:
                     resolvents.add(frozenset(new_clause))
         return resolvents
 
-    def resolution(self, query):
-        cnf = to_cnf(And(*self.knowledge_base, Not(query)))
+    def resolution(self, query, knowledge_base=None):
+        if knowledge_base is None:
+            cnf = to_cnf(And(*self.knowledge_base, Not(query)))
+        else:
+            cnf = to_cnf(And(*knowledge_base, Not(query)))
         clauses = to_int_repr(cnf.args, cnf.free_symbols)
         clauses = set([frozenset(x) for x in clauses])
         new_clauses = set()
@@ -37,24 +45,65 @@ class Agent:
                 return False
             clauses |= new_clauses
 
-    def entailment(self, proposition, query):
-        agent = Agent()
-        agent.tell(proposition)
-        return agent.ask(query)
+    def entailment(self, knowledge_base, query):
+        return self.resolution(query=query, knowledge_base=knowledge_base)
+
+    def remainders(self, set_a, phi):
+        set_a = to_cnf(And(*set_a))
+        #set_a_list = to_int_repr(set_a.args, set_a.free_symbols)
+        set_a_list = set_a.args
+        set_a_subsets = list(chain.from_iterable(combinations(set_a_list, r) for r in range(1, len(set_a_list))))
+        set_a_subsets.sort(key=len, reverse=True)
+
+        remainders = list()
+        for set_a_subset in set_a_subsets:
+            set_a_subset = frozenset(set_a_subset)
+            if not self.entailment(set_a_subset, phi) and not any([set_a_subset.issubset(r) for r in remainders]):
+                remainders.append(set_a_subset)
+
+        #for remainder in remainders:
+        #    if not any([remainder.issubset(r) for r in remainders if r != remainder]):
+        #        remainders.remove(remainder)
+
+        return remainders
+
 
     def partial_meet_contraction(self, query):
+        print('Contracting', self.knowledge_base, 'with', query)
+        remainders = self.remainders(set_a=self.knowledge_base, phi=query)
+        self.knowledge_base = set.intersection(*[set(fs) for fs in remainders])
+        print('New knowledge base:', self.knowledge_base)
+
+    def partial_meet_contraction_samy(self, query):
+        print('SAMYs METHOD: Contracting', self.knowledge_base, 'with', query)
         new_knowledge_base = set()
-        for proposition in self.knowledge_base:
-            if not self.entailment(proposition, query):
-                new_knowledge_base.add(proposition)
+        clauses = to_cnf(And(*self.knowledge_base)).args
+        for clause in clauses:
+            clause_set = set()
+            clause_set.add(clause)
+            if not self.entailment(clause_set, query):
+                new_knowledge_base.add(clause)
         self.knowledge_base = new_knowledge_base
+        print('New knowledge base:', self.knowledge_base)
+
+    def revision(self, query):
+        self.partial_meet_contraction(Not(query))
+        self.tell(query)
+
 
 # tests
 agent = Agent()
 A, B, C = symbols('A B C')
 agent.tell(And(A, B))
 agent.tell(Or(A, B))
-print(agent.ask(B))
-print(agent.ask(C))
+print('Does', agent.knowledge_base, 'entails', B, '?', agent.ask(B))
+print('Does', agent.knowledge_base, 'entails', C, '?', agent.ask(C))
 agent.partial_meet_contraction(B)
-print(agent.knowledge_base)
+
+
+agent = Agent()
+A, B, C = symbols('A B C')
+agent.tell(And(A, B))
+agent.tell(Or(A, B))
+agent.partial_meet_contraction_samy(B)
+# print(agent.knowledge_base)
