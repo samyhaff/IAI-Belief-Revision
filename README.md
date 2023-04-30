e188a4368adbda114e98b332bc8fd6ae24dd7ac7
andreabosisio <andrea2.bosisio@mail.polimi.it>
2023-04-30 13:24:24 +0200

    Resolution bug fix + added tests (see output on the README.md)

--- README.md
+++ README.md
@@ -1,1 +1,86 @@
 # Belief Revision Agent
+## Output
+```
+Agent now beliefs that A & B
+Agent 's new knowledge base: [A & B]
+Agent now beliefs that A | B
+Agent 's new knowledge base: [A & B, A | B]
+Does [A & B, A | B] entails B ? True
+Does [A & B, A | B] entails C ? False
+Agent is revising [A & B, A | B] with ~B
+Agent 's new knowledge base after contraction: [A | B, A]
+Agent now beliefs that ~B
+Agent 's new knowledge base: [A | B, A, ~B]
+Agent 's updated knowledge base: [A | B, A, ~B]
+
+Agent now beliefs that p
+Agent 's new knowledge base: [p]
+Agent now beliefs that q
+Agent 's new knowledge base: [p, q]
+Agent now beliefs that r
+Agent 's new knowledge base: [p, q, r]
+Agent is revising [p, q, r] with ~(q | r)
+Agent 's new knowledge base after contraction: [p]
+Agent now beliefs that ~(q | r)
+Agent 's new knowledge base: [p, ~(q | r)]
+Agent 's updated knowledge base: [p, ~(q | r)]
+
+Agent now beliefs that p
+Agent 's new knowledge base: [p]
+Agent now beliefs that q
+Agent 's new knowledge base: [p, q]
+Agent now beliefs that q | ~p
+Agent 's new knowledge base: [p, q, q | ~p]
+Agent is revising [p, q, q | ~p] with ~q
+Agent 's new knowledge base after contraction: [p]
+Agent now beliefs that ~q
+Agent 's new knowledge base: [p, ~q]
+Agent 's updated knowledge base: [p, ~q]
+
+Agent now beliefs that q | ~p
+Agent 's new knowledge base: [q | ~p]
+Agent now beliefs that p
+Agent 's new knowledge base: [q | ~p, p]
+Agent now beliefs that q
+Agent 's new knowledge base: [q | ~p, p, q]
+Agent is revising [q | ~p, p, q] with ~q
+Agent 's new knowledge base after contraction: [q | ~p]
+Agent now beliefs that ~q
+Agent 's new knowledge base: [q | ~p, ~q]
+Agent 's updated knowledge base: [q | ~p, ~q]
+
+Agent now beliefs that p
+Agent 's new knowledge base: [p]
+Agent now beliefs that q
+Agent 's new knowledge base: [p, q]
+Agent is revising [p, q] with ~q
+Agent 's new knowledge base after contraction: [p]
+Agent now beliefs that ~q
+Agent 's new knowledge base: [p, ~q]
+Agent 's updated knowledge base: [p, ~q]
+
+Alice now beliefs that p
+Alice 's new knowledge base: [p]
+Alice now beliefs that q
+Alice 's new knowledge base: [p, q]
+Bob now beliefs that p
+Bob 's new knowledge base: [p]
+Bob now beliefs that (p | ~q) & (q | ~p)
+Bob 's new knowledge base: [p, (p | ~q) & (q | ~p)]
+Alice is revising [p, q] with ~p
+Alice 's new knowledge base after contraction: [q]
+Alice now beliefs that ~p
+Alice 's new knowledge base: [q, ~p]
+Alice 's updated knowledge base: [q, ~p]
+
+Bob is revising [p, (p | ~q) & (q | ~p)] with ~p
+fuck
+Bob 's new knowledge base after contraction: [p | ~q, q | ~p]
+Bob now beliefs that ~p
+Bob 's new knowledge base: [p | ~q, q | ~p, ~p]
+Bob 's updated knowledge base: [p | ~q, q | ~p, ~p]
+
+
+Process finished with exit code 0
+
+```
--- agent.py
+++ agent.py
@@ -5,7 +5,8 @@ from itertools import combinations
 
 
 class Agent:
-    def __init__(self):
+    def __init__(self, name="Agent"):
+        self.name = name
         self.knowledge_base = list()
 
     def get_clauses(self, expr):
@@ -14,11 +15,11 @@ class Agent:
         return set(expr.args)
 
     def tell(self, sentence):
-        #print('Adding', sentence, 'as', to_cnf(sentence))
-        #self.knowledge_base.add(to_cnf(sentence))
-        print('Adding', sentence)
+        # print('Adding', sentence, 'as', to_cnf(sentence))
+        # self.knowledge_base.add(to_cnf(sentence))
+        print(self.name, 'now beliefs that', sentence)
         self.knowledge_base.append(sentence)
-        print('New knowledge base:', self.knowledge_base)
+        print(self.name, '\'s new knowledge base:', self.knowledge_base)
 
     def ask(self, query):
         return self.resolution(query)
@@ -28,7 +29,9 @@ class Agent:
         for literal1 in clause1:
             for literal2 in clause2:
                 if literal1 == - literal2:
-                    new_clause = clause1.union(clause2) - {literal1, literal2}
+                    new_clause1 = clause1 - {literal1}
+                    new_clause2 = clause2 - {literal2}
+                    new_clause = new_clause1.union(new_clause2)
                     resolvents.add(frozenset(new_clause))
         return resolvents
 
@@ -40,7 +43,7 @@ class Agent:
         clauses = to_int_repr(cnf.args, cnf.free_symbols)
         if isinstance(cnf, Or):
             clauses = [set.union(*clauses)]
-        #print('Clauses:', clauses)
+        # print('Clauses:', clauses)
         clauses = set([frozenset(x) for x in clauses])
         new_clauses = set()
         while True:
@@ -58,16 +61,17 @@ class Agent:
         return self.resolution(query=query, knowledge_base=knowledge_base)
 
     def remainders(self, set_a, phi):
-        #set_a = to_cnf(And(*set_a)) # change the order of the KB, so we are losing the temporal order of the formula
-        #set_a_list = set_a.args
+        # set_a = to_cnf(And(*set_a)) # change the order of the KB, so we are losing the temporal order of the formula
+        # set_a_list = set_a.args
         set_a_list = set_a
-        #remainders = set(copy.deepcopy(set_a_list))
+        # remainders = set(copy.deepcopy(set_a_list))
 
         if not self.entailment(knowledge_base=set_a, query=phi):
             return set_a
 
         for i in range(1, len(set_a_list)):
-            to_remove_formulas = [x[0] if len(x) == 1 else x for x in combinations(set_a_list, i)] #check if the order make sense (YES: it discard before the last added formulas/clause)
+            to_remove_formulas = [x[0] if len(x) == 1 else x for x in combinations(set_a_list,
+                                                                                   i)]  # check if the order make sense (YES: it discard before the last added formulas/clause)
             for to_remove_formula in reversed(to_remove_formulas):
                 for to_remove_clause in self.get_clauses(to_cnf(to_remove_formula)):
                     if isinstance(to_remove_clause, sympy.Tuple):
@@ -83,14 +87,12 @@ class Agent:
         remainders = self.remainders(set_a=self.knowledge_base, phi=query)
         self.knowledge_base = remainders
 
-
     def partial_meet_contraction(self, query):
         print('Contracting', self.knowledge_base, 'with', query)
         remainders = self.remainders(set_a=self.knowledge_base, phi=query)
         if len(remainders) > 0:
             # self.knowledge_base = set.intersection(*[set(fs) for fs in remainders]) # doesn't always work
-            self.knowledge_base = set(remainders[0]) # testing purposes
-
+            self.knowledge_base = set(remainders[0])  # testing purposes
 
     def partial_meet_contraction_samy(self, query):
         new_knowledge_base = set()
@@ -102,16 +104,13 @@ class Agent:
                 new_knowledge_base.add(clause)
         self.knowledge_base = new_knowledge_base
 
-
     def revision(self, query, contraction_function):
-        print('Revising', self.knowledge_base, 'with', query)
+        print(self.name, 'is revising', self.knowledge_base, 'with', query)
         contraction_function(Not(query))
-        print('New knowledge base after contraction:', self.knowledge_base)
+        print(self.name, '\'s new knowledge base after contraction:', self.knowledge_base)
         self.tell(query)
-        print('Updated knowledge base:', self.knowledge_base)
+        print(self.name, '\'s updated knowledge base:', self.knowledge_base)
         print()
-
-
 
 
 # tests
@@ -121,7 +120,6 @@ print(agent.entailment(knowledge_base=None, query=Or(A, Not(A))))
 agent = Agent()
 print(agent.entailment(knowledge_base=None, query=And(A, Not(A))))
 
-
 agent = Agent()
 A, B, C = symbols('A B C')
 agent.tell(And(A, B))
@@ -130,7 +128,6 @@ print('Does', agent.knowledge_base, 'entails', B, '?', agent.ask(B))
 print('Does', agent.knowledge_base, 'entails', C, '?', agent.ask(C))
 agent.revision(Not(B), contraction_function=agent.contraction)
 
-
 agent = Agent()
 p, q, r = symbols('p q r')
 agent.tell(p)
@@ -138,17 +135,42 @@ agent.tell(q)
 agent.tell(r)
 agent.revision(Not(Or(q, r)), contraction_function=agent.contraction)
 
-
 agent = Agent()
-p, q = symbols('p, q')
+p, q = symbols('p q')
 agent.tell(p)
 agent.tell(q)
 agent.tell(Or(Not(p), q))
 agent.revision(Not(q), contraction_function=agent.contraction)
 
 agent = Agent()
-p, q = symbols('p, q')
+p, q = symbols('p q')
 agent.tell(Or(Not(p), q))
 agent.tell(p)
 agent.tell(q)
 agent.revision(Not(q), contraction_function=agent.contraction)
+
+agent = Agent()
+p, q = symbols('p q')
+agent.tell(p)
+agent.tell(q)
+agent.revision(Not(q), contraction_function=agent.contraction)
+
+alice = Agent("Alice")
+p, q = symbols('p q')
+alice.tell(p)
+alice.tell(q)
+
+bob = Agent("Bob")
+p, q = symbols('p q')
+bob.tell(p)
+bob.tell(And(
+    Or(Not(p), q),
+    Or(Not(q), p)
+    )
+)
+
+alice.revision(Not(p), alice.contraction)
+bob.revision(Not(p), bob.contraction)
+
+
+
